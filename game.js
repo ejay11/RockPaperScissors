0eab90495c2efb582527c78ed5d39604a0786ad1
ejay11 <ellen.jaquette@gmail.com>
2021-05-17 21:15:33 -0500

    playRound function complete

--- game.js
+++ game.js
@@ -3,7 +3,9 @@ console.log("Hello World");
 
 let playerSelection = prompt("rock, paper, or scissors?");
 
-// Change case on playerSelection to capital, then lowercase
+// Change case on playerSelection to lowercase
+
+playerSelection = playerSelection.toLowerCase();
 
 //Computer Selection Function
 function computerPlay() {
@@ -18,12 +20,12 @@ function computerPlay() {
 
 // playRound function - compare results
 function playRound(playerSelection, computerSelection) {
-     if (playerSelection === 'rock' && computerSelection === 'rock') {
-         return 'You both choose rock! It\'s a tie!';
-     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-         return 'You win!';
+     if (playerSelection === computerSelection) {
+         return 'You both choose ' + playerSelection + '! It\'s a tie!';
+     } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock')) {
+         return 'You win! ' + playerSelection + ' beats ' + computerSelection + '!';
      } else {
-         return 'Sorry, you lost! Please try again.';
+         return 'Sorry, you lost! ' + computerSelection + ' beats ' + playerSelection + '. Please try again.';
      }
   }
 
@@ -36,8 +38,6 @@ console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
 // Comparison of selections and determination of winner
-// If player selects rock, and computer selects rock
-// If player selects rock, and computer selects scissors 
 // If player selects rock, and computer selects paper
 
 
@@ -45,9 +45,8 @@ console.log(playRound(playerSelection, computerSelection));
 
 
     // If player selects paper, and computer selects rock
-    // If player selects paper, and computer selects paper
     // If player selects paper, and computer selects scissors
 
-    // If player selects scissors, and computer selects rock
+    
     // If play selects scissors, and computer selects paper
     // If player selects scissors, and computer selects scissors
